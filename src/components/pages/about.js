55a855d489facfe76ec0c8b7cdc8edb9360ba644
Siamak Samie <siamak.samie@gmail.com>
2019-01-04 04:51:41 -0500

    cleaning

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -2,21 +2,7 @@ import React, { Component } from "react";
 
 class About extends Component {
   render() {
-    return (
-      <div className="container-fluid">
-        <div className="col-md-12">
-          <h2>Is she dead, yes or no?</h2>
-          <p>
-            Well, the way they make shows is, they make one show. That show's
-            called a pilot. Then they show that show to the people who make
-            shows, and on the strength of that one show they decide if they're
-            going to make more shows. Some pilots get picked and become
-            television programs. Some don't, become nothing. She starred in one
-            of the ones that became nothing.{" "}
-          </p>
-        </div>
-      </div>
-    );
+    return <div className="container-fluid">test</div>;
   }
 }
 
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -2,76 +2,7 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    return (
-      <div className="container-fluid">
-        <div className="col-md-4">
-          <h2>Is she dead, yes or no?</h2>
-          <p>
-            Well, the way they make shows is, they make one show. That show's
-            called a pilot. Then they show that show to the people who make
-            shows, and on the strength of that one show they decide if they're
-            going to make more shows. Some pilots get picked and become
-            television programs. Some don't, become nothing. She starred in one
-            of the ones that became nothing.{" "}
-          </p>
-        </div>
-        <div className="col-md-4">
-          <h2>Is she dead, yes or no?</h2>
-          <p>
-            Well, the way they make shows is, they make one show. That show's
-            called a pilot. Then they show that show to the people who make
-            shows, and on the strength of that one show they decide if they're
-            going to make more shows. Some pilots get picked and become
-            television programs. Some don't, become nothing. She starred in one
-            of the ones that became nothing.{" "}
-          </p>
-        </div>
-        <div className="col-md-4">
-          <h2>Is she dead, yes or no?</h2>
-          <p>
-            Well, the way they make shows is, they make one show. That show's
-            called a pilot. Then they show that show to the people who make
-            shows, and on the strength of that one show they decide if they're
-            going to make more shows. Some pilots get picked and become
-            television programs. Some don't, become nothing. She starred in one
-            of the ones that became nothing.{" "}
-          </p>
-        </div>
-        <div className="col-md-4">
-          <h2>Is she dead, yes or no?</h2>
-          <p>
-            Well, the way they make shows is, they make one show. That show's
-            called a pilot. Then they show that show to the people who make
-            shows, and on the strength of that one show they decide if they're
-            going to make more shows. Some pilots get picked and become
-            television programs. Some don't, become nothing. She starred in one
-            of the ones that became nothing.{" "}
-          </p>
-        </div>
-        <div className="col-md-4">
-          <h2>Is she dead, yes or no?</h2>
-          <p>
-            Well, the way they make shows is, they make one show. That show's
-            called a pilot. Then they show that show to the people who make
-            shows, and on the strength of that one show they decide if they're
-            going to make more shows. Some pilots get picked and become
-            television programs. Some don't, become nothing. She starred in one
-            of the ones that became nothing.{" "}
-          </p>
-        </div>
-        <div className="col-md-4">
-          <h2>Is she dead, yes or no?</h2>
-          <p>
-            Well, the way they make shows is, they make one show. That show's
-            called a pilot. Then they show that show to the people who make
-            shows, and on the strength of that one show they decide if they're
-            going to make more shows. Some pilots get picked and become
-            television programs. Some don't, become nothing. She starred in one
-            of the ones that became nothing.{" "}
-          </p>
-        </div>
-      </div>
-    );
+    return <div className="container-fluid">test</div>;
   }
 }
 
