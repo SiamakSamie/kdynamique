4a96d27d65e427b5e66d22b94abba942521ad0bc
Siamak Samie <siamak.samie@gmail.com>
2019-03-02 05:21:49 -0500

    footer stuff

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,9 +3,18 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <footer className="footer">
-        <p>Footer</p>
-      </footer>
+      <div className="footer">
+        <h2>
+          <i className="fas fa-angle-double-up" />
+        </h2>
+        <div id="container">
+          <div id="cont">
+            <div className="footer_center">
+              <h3>Footer Content</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
