e4d0b6fa884a1f3458bb0f50d4aeaada44cd1e8b
Siamak Samie <siamak.samie@gmail.com>
2019-03-09 00:53:30 -0500

    small fixes

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -3,101 +3,73 @@ import React, { Component } from "react";
 class Contact extends Component {
   render() {
     return (
-      <div classNameName="container-fluid">
-        <form
-          class="form-horizontal"
-          action=" "
-          method="post"
-          id="contact_form"
-        >
+      <div className="container-fluid">
+        <form className="form-horizontal" action=" " method="post" id="contact_form">
           <fieldset>
             <legend>Something something something yay</legend>
 
-            <div class="form-group">
-              <div class="col-md-12 inputGroupContainer">
-                <div class="input-group">
-                  <span class="input-group-addon">
-                    <div class="buffer">
-                      <i class="fas fa-user" />
-                      <p class="addon-text">Name</p>
+            <div className="form-group">
+              <div className="col-md-12 inputGroupContainer">
+                <div className="input-group">
+                  <span className="input-group-addon">
+                    <div className="buffer">
+                      <i className="fas fa-user" />
+                      <p className="addon-text">Name</p>
                     </div>
                   </span>
-                  <input
-                    name="name"
-                    placeholder="Name"
-                    class="form-control"
-                    type="text"
-                    required
-                  />
+                  <input name="name" placeholder="Name" className="form-control" type="text" required />
                 </div>
               </div>
             </div>
 
-            <div class="form-group">
-              <div class="col-md-12 inputGroupContainer">
-                <div class="input-group">
-                  <span class="input-group-addon">
-                    <div class="buffer">
-                      <i class="fas fa-at" />
-                      <p class="addon-text">E-mail</p>
+            <div className="form-group">
+              <div className="col-md-12 inputGroupContainer">
+                <div className="input-group">
+                  <span className="input-group-addon">
+                    <div className="buffer">
+                      <i className="fas fa-at" />
+                      <p className="addon-text">E-mail</p>
                     </div>
                   </span>
-                  <input
-                    name="email"
-                    placeholder="E-Mail Address"
-                    class="form-control"
-                    type="text"
-                    required
-                  />
+                  <input name="email" placeholder="E-Mail Address" className="form-control" type="text" required />
                 </div>
               </div>
             </div>
 
-            <div class="form-group">
-              <div class="col-md-12 inputGroupContainer">
-                <div class="input-group">
-                  <span class="input-group-addon">
-                    <div class="buffer">
-                      <i class="fas fa-phone" />
-                      <p class="addon-text">Phone</p>
+            <div className="form-group">
+              <div className="col-md-12 inputGroupContainer">
+                <div className="input-group">
+                  <span className="input-group-addon">
+                    <div className="buffer">
+                      <i className="fas fa-phone" />
+                      <p className="addon-text">Phone</p>
                     </div>
                   </span>
-                  <input
-                    name="phone"
-                    placeholder="(845)555-1212"
-                    class="form-control"
-                    type="text"
-                    required
-                  />
+                  <input name="phone" placeholder="(845)555-1212" className="form-control" type="text" required />
                 </div>
               </div>
             </div>
 
-            <div class="form-group">
-              <div class="col-md-12 inputGroupContainer">
-                <div class="input-group">
-                  <span class="input-group-addon">
-                    <div class="buffer">
-                      <i class="fas fa-pencil-alt" />
-                      <p class="addon-text">Message</p>
+            <div className="form-group">
+              <div className="col-md-12 inputGroupContainer">
+                <div className="input-group">
+                  <span className="input-group-addon">
+                    <div className="buffer">
+                      <i className="fas fa-pencil-alt" />
+                      <p className="addon-text">Message</p>
                     </div>
                   </span>
-                  <textarea
-                    class="form-control"
-                    name="comment"
-                    placeholder="Project Description"
-                    required
-                  />
+                  <textarea className="form-control" name="comment" placeholder="Project Description" required />
                 </div>
               </div>
             </div>
 
-            <div class="form-group">
-              <div class="col-md-12">
+            <div className="form-group">
+              <div className="col-md-12">
                 <button className="btn btn-primary">
                   <span className="btn-content">send</span>
                   <span className="icon">
-                    <i class="fas fa-paper-plane" />
+                    <i className="fas fa-paper-plane" />
                   </span>
                 </button>
               </div>
