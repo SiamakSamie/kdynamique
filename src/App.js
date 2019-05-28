442af9bfdf08440bccf68b8d3c2b35495c4a1f3a
Siamak Samie <siamak.samie@gmail.com>
2019-05-28 00:51:42 -0400

    better services and added fb messenger

--- src/App.js
+++ src/App.js
@@ -35,6 +35,24 @@ class App extends Component {
       { label: "Contact Us", link: "/contact" }
     ];
 
+    window.fbAsyncInit = function() {
+      const FB = window.FB;
+      FB.init({
+        xfbml: true,
+        version: "v3.3"
+      });
+    };
+
+    (function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+
     return (
       <Router basename={process.env.PUBLIC_URL}>
         <ScrollToTop>
@@ -47,6 +65,10 @@ class App extends Component {
             <Route exact path="/contact" component={Contact} />
             <Route exact path="/portfolio" component={Portfolio} />
             <Footer />
+
+            <div id="fb-root" />
+
+            <div class="fb-customerchat" attribution="setup_tool" page_id="156784661430604" />
           </div>
         </ScrollToTop>
       </Router>
--- src/components/pages/services.js
+++ src/components/pages/services.js
@@ -2,72 +2,58 @@ import React, { Component } from "react";
 
 class Services extends Component {
   render() {
+    let services = [
+      {
+        title: "Video Production",
+        logo: "fas fa-pencil-alt fa-3x",
+        description:
+          "I am too busy trying to make shit pretty to even come up with decent placeholder text. Someone better help me with part or imma cut a bitch"
+      },
+      {
+        title: "Video Production",
+        logo: "fas fa-pencil-alt fa-3x",
+        description:
+          "I am too busy trying to make shit pretty to even come up with decent placeholder text. Someone better help me with part or imma cut a bitch"
+      },
+      {
+        title: "Video Production",
+        logo: "fas fa-pencil-alt fa-3x",
+        description:
+          "I am too busy trying to make shit pretty to even come up with decent placeholder text. Someone better help me with part or imma cut a bitch"
+      },
+      {
+        title: "Video Production",
+        logo: "fas fa-pencil-alt fa-3x",
+        description:
+          "I am too busy trying to make shit pretty to even come up with decent placeholder text. Someone better help me with part or imma cut a bitch"
+      },
+      {
+        title: "Video Production",
+        logo: "fas fa-pencil-alt fa-3x",
+        description:
+          "I am too busy trying to make shit pretty to even come up with decent placeholder text. Someone better help me with part or imma cut a bitch"
+      },
+      {
+        title: "Video Production",
+        logo: "fas fa-pencil-alt fa-3x",
+        description:
+          "I am too busy trying to make shit pretty to even come up with decent placeholder text. Someone better help me with part or imma cut a bitch"
+      }
+    ];
+
+    let service = services.map((object, i) => (
+      <div className="col-sm-4">
+        <div className=" box">
+          <i className={object.logo} />
+          <h4>{object.title}</h4>
+          <p>{object.description}</p>
+        </div>
+      </div>
+    ));
+
     return (
       <div className="services">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm-4">
-              <div className=" box">
-                <i className="fas fa-pencil-alt fa-3x" />
-                <h4>Video Production</h4>
-                <p>
-                  I am too busy trying to make shit pretty to even come up with decent placeholder text. Someone better help me with part or imma cut
-                  a bitch
-                </p>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className=" box">
-                <i className="fas fa-video fa-3x" />
-                <h4>Content Development</h4>
-                <p>
-                  I am too busy trying to make shit pretty to even come up with decent placeholder text. Someone better help me with part or imma cut
-                  a bitch
-                </p>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className=" box">
-                <i className="fas fa-desktop fa-3x" />
-                <h4>Live Events</h4>
-                <p>
-                  I am too busy trying to make shit pretty to even come up with decent placeholder text. Someone better help me with part or imma cut
-                  a bitch
-                </p>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className=" box">
-                <i className="fas fa-pencil-alt fa-3x" />
-                <h4>Video Production</h4>
-                <p>
-                  I am too busy trying to make shit pretty to even come up with decent placeholder text. Someone better help me with part or imma cut
-                  a bitch
-                </p>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className=" box">
-                <i className="fas fa-pencil-alt fa-3x" />
-                <h4>Video Production</h4>
-                <p>
-                  I am too busy trying to make shit pretty to even come up with decent placeholder text. Someone better help me with part or imma cut
-                  a bitch
-                </p>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className=" box">
-                <i className="fas fa-pencil-alt fa-3x" />
-                <h4>Video Production</h4>
-                <p>
-                  I am too busy trying to make shit pretty to even come up with decent placeholder text. Someone better help me with part or imma cut
-                  a bitch
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div className="container-fluid">{service}</div>
       </div>
     );
   }
