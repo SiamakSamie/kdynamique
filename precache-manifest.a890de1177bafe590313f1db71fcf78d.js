9b31f67c076e57d2ecdee995b159f4578a557ae1
Siamak Samie <siamak.samie@gmail.com>
2022-06-22 17:07:01 -0400

    Updates

---
+++ precache-manifest.a890de1177bafe590313f1db71fcf78d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "33b66e6f86b9f5248fd180b54e4e4967",
+    "url": "/static/media/rode.33b66e6f.png"
+  },
+  {
+    "revision": "743029bd3857c3bdfb27",
+    "url": "/static/css/main.87039aab.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5d24eb8db2be1d7a958f",
+    "url": "/static/js/2.d3068b9d.chunk.js"
+  },
+  {
+    "revision": "3013c6c0b01f6ba348e509e5dfc9f5fc",
+    "url": "/static/media/home.3013c6c0.JPG"
+  },
+  {
+    "revision": "b07d53cda184a8532f3e37e1e9a8b661",
+    "url": "/static/media/portfolio.b07d53cd.jpg"
+  },
+  {
+    "revision": "b8c573342ec2be06084e2a0a085edf3c",
+    "url": "/static/media/about.b8c57334.jpg"
+  },
+  {
+    "revision": "e1de3841f4536ec2dcc378fe2df0246e",
+    "url": "/static/media/contactUs.e1de3841.jpg"
+  },
+  {
+    "revision": "bf12f79f1ffceefadff1ab718aaac368",
+    "url": "/static/media/kdyLogoWhite.bf12f79f.png"
+  },
+  {
+    "revision": "d594953b467c6ab84ea0a09521d6e46b",
+    "url": "/static/media/fantasia.d594953b.jpg"
+  },
+  {
+    "revision": "743029bd3857c3bdfb27",
+    "url": "/static/js/main.ee25b687.chunk.js"
+  },
+  {
+    "revision": "27aa43f0bb8734b24e4dca90b786de3f",
+    "url": "/static/media/48h.27aa43f0.png"
+  },
+  {
+    "revision": "48cd815ebab33e0a7d5b9b88454c31ab",
+    "url": "/static/media/actionon.48cd815e.png"
+  },
+  {
+    "revision": "5d458c451c8f2619611bfb195261034f",
+    "url": "/static/media/crank.5d458c45.png"
+  },
+  {
+    "revision": "b5c0959aca8e72f60a981861d88631df",
+    "url": "/static/media/kdyTextBlack.b5c0959a.png"
+  },
+  {
+    "revision": "d12cd6bbaf5df12153367082099a5063",
+    "url": "/static/media/malik.d12cd6bb.png"
+  },
+  {
+    "revision": "d9d006a3235d0a4d28f93ffda1402b55",
+    "url": "/static/media/siamak.d9d006a3.jpg"
+  },
+  {
+    "revision": "cb406510d6d27395e11a561c57830014",
+    "url": "/static/media/frank.cb406510.jpg"
+  },
+  {
+    "revision": "6fd55e9ebf36075e34283cbb2331e489",
+    "url": "/static/media/Capsmall.6fd55e9e.ttf"
+  },
+  {
+    "revision": "074b55213fe4bbe00c868e32aa85398d",
+    "url": "/index.html"
+  }
+];
